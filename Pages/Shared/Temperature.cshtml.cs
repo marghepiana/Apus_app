b306d6e6080e1e151e6ff53f66f8e1798bba178d
marghepiana <114315654+marghepiana@users.noreply.github.com>
2023-08-14 18:42:21 -0400

    Add files via upload

---
+++ Pages/Shared/Temperature.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Apus_app.Pages;
+
+public class TemperatureModel : PageModel
+{
+    private readonly ILogger<TemperatureModel> _logger;
+
+    public TemperatureModel(ILogger<TemperatureModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
